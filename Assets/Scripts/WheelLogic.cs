25dcb6486438f298cc61d5d1dea6f3a1fe3a03c6
cesar <cesaroumeres@gmail.com>
2024-04-08 11:15:29 -0500

    antes de la correccion del texto

---
+++ Assets/Scripts/WheelLogic.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyUI.PickerWheelUI;
+using UnityEngine.UI;
+using TMPro;
+
+public class WheelLogic : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI spinTextButton;
+    [SerializeField] Button spinButton;
+    [SerializeField] PickerWheel pickerWheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spinButton.onClick.AddListener (() => {
+           spinButton.interactable = false;
+           spinTextButton.text = "Girando";
+
+           pickerWheel.OnSpinStart(()=> Debug.Log("el giro a comenzado"));
+
+           pickerWheel.OnSpinEnd(wheelPiece =>{
+            Debug.Log("Spin end: Label:" + wheelPiece.Label + "Amount" + wheelPiece.Amount );
+            spinButton.interactable = true;
+            spinTextButton.text = "presiona para jugar";
+           });
+           pickerWheel.Spin();
+        });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
